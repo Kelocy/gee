6c217dde252c3699ece86b076c80d2f62a36ebaa
Kelocy <ceolhx@outlook.com>
2024-01-30 10:36:06 +0900

    finish hooks for gee_orm

---
+++ gee_orm/5_hooks/schema/schema_test.go
@@ -0,0 +1,25 @@
+package schema
+
+import (
+	"fmt"
+	"geeorm/dialect"
+	"testing"
+)
+
+type User struct {
+	Name string `geeorm:"PRIMARY KEY"`
+	Age  int
+}
+
+var TestDial, _ = dialect.GetDialect("sqlite3")
+
+func TestParse(t *testing.T) {
+	schema := Parse(&User{}, TestDial)
+	if schema.Name != "User" || len(schema.Fields) != 2 {
+		fmt.Println(schema.Name)
+		t.Fatal("failed to parse User struct")
+	}
+	if schema.GetField("Name").Tag != "PRIMARY KEY" {
+		t.Fatal("failed to parse primary key")
+	}
+}
