a0b507e53d5d40c805343a0cb4e19b26ef92918d
Kelocy <ceolhx@outlook.com>
2024-02-05 17:18:27 +0900

    finish service for gee_rpc

--- gee_rpc/3_service/service.go
+++ gee_rpc/3_service/service.go
@@ -22,14 +22,14 @@ func (m *methodType) newArgv() reflect.Value {
 	var argv reflect.Value
 	// arg may be a pointer type, or a value type
 	if m.ArgType.Kind() == reflect.Ptr {
-		argv = reflect.New(m.ArgType.Elem())
+		argv = reflect.New(m.ArgType.Elem()) // create a pointer
 	} else {
-		argv = reflect.New(m.ArgType).Elem()
+		argv = reflect.New(m.ArgType).Elem() // create a value
 	}
 	return argv
 }
 
-func (m *methodType) newReply() reflect.Value {
+func (m *methodType) newReplyv() reflect.Value {
 	// reply must be a pointer type
 	replyv := reflect.New(m.ReplyType.Elem())
 	switch m.ReplyType.Elem().Kind() {
@@ -54,12 +54,46 @@ func newService(rcvr interface{}) *service {
 	s.name = reflect.Indirect(s.rcvr).Type().Name()
 	s.typ = reflect.TypeOf(rcvr)
 	if !ast.IsExported(s.name) {
-		log.Fatal("rpc server: %s is not a valid service name", s.name)
+		log.Fatalf("rpc server: %s is not a valid service name", s.name)
 	}
 	s.registerMethods()
 	return s
 }
 
 func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc server: register %s.%s", s.name, method.Name)
+	}
+}
 
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
 }
